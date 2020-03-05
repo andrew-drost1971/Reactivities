21541dfac610688d7da21fc78016e7af09cfffb6
Drost <j_andrew.drost@bell.corp.bce.ca>
2020-03-05 14:20:36 -0400

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace API
@@ -23,6 +17,8 @@ namespace API
                 ///try {
                     var context = services.GetRequiredService<DataContext>();
                     context.Database.Migrate();
+                    Seed.SeedData(context);
+                    
                 //}
                 //catch (Exception ex)
                 //{
