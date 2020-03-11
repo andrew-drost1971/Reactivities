9274fe2e1d25a0cce997e96540a36768da3f0164
Drost <j_andrew.drost@bell.corp.bce.ca>
2020-03-11 11:18:43 -0300

    End of Section 5

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -11,28 +11,48 @@ interface IProps {
   selectedActivity: IActivity | null;
   editMode: boolean;
   setEditMode: (editMode: boolean) => void;
-  setSelectedActivity: (activity: IActivity | null) => void 
+  setSelectedActivity: (activity: IActivity | null) => void;
+  createActivity: (activity: IActivity) => void;
+  editActivity: (activity: IActivity) => void;
+  deleteActivity: (id: string) => void;
 }
 
-export const ActivityDashboard: React.FC<IProps> = ({ 
-  activities, 
-  selectActivity, 
-  selectedActivity, 
-  editMode,  
+export const ActivityDashboard: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  selectedActivity,
+  editMode,
   setEditMode,
-  setSelectedActivity}) => {
+  setSelectedActivity,
+  createActivity,
+  editActivity,
+  deleteActivity
+}) => {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <ActivityList activities={activities} selectActivity={selectActivity}/>
+        <ActivityList activities={activities} selectActivity={selectActivity} 
+            deleteActivity= {deleteActivity}/>
       </Grid.Column>
       <Grid.Column width={6}>
-          {selectedActivity && !editMode && (
-          <ActivityDetails activity={selectedActivity} setEditMode={setEditMode} setSelectedActivity={setSelectedActivity}/>)}
-       {editMode && <ActivityForm  activity={selectedActivity!} setEditMode={setEditMode} />}
-       
-      </Grid.Column>
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            setEditMode={setEditMode}
+            setSelectedActivity={setSelectedActivity}
+          />
+        )}
+        {editMode && (
+          <ActivityForm
+            key={selectedActivity && (selectedActivity.id || 0)}
+            activity={selectedActivity!}
+            setEditMode={setEditMode}
+            createActivity={createActivity}
+            editActivity={editActivity}
 
+          />
+        )}
+      </Grid.Column>
     </Grid>
   );
 };
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,15 +1,20 @@
 import React, { useState, FormEvent } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
+import {v4 as uuid} from 'uuid';
 
 interface IProps {
   activity: IActivity;
   setEditMode: (editMode: boolean) => void;
+  createActivity: (activity: IActivity) => void;
+  editActivity: (activity: IActivity) => void;
 }
 
 const ActivityForm: React.FC<IProps> = ({
   activity: initialFormState,
-  setEditMode
+  setEditMode,
+  createActivity,
+  editActivity
 }) => {
   const intitializeForm = () => {
     if (initialFormState) {
@@ -30,10 +35,20 @@ const ActivityForm: React.FC<IProps> = ({
   const [activity, setActivity] = useState<IActivity>(intitializeForm);
 
   const handleSubmit = () => {
-    console.log(activity);
-  }
+    if (activity.id.length === 0) {
+      let newActivity = {
+        ...activity,
+        id: uuid()
+      };
+      createActivity(newActivity);
+    } else {
+      editActivity(activity);
+    }
+  };
 
-  const handleInputChange = (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleInputChange = (
+    event: FormEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = event.currentTarget;
     setActivity({ ...activity, [name]: value });
   };
@@ -55,7 +70,7 @@ const ActivityForm: React.FC<IProps> = ({
           name="description"
         />
         <Form.Input
-          type="date"
+          type="datetime-local"
           placeholder="Date"
           value={activity.date}
           onChange={handleInputChange}
